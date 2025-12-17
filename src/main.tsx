7519e132aec8ba84b27c07928b68a4584cdbbdea
ptitsuissee <xanos26@gmail.com>
2025-12-17 17:57:21 +0100

    Integrate App component with React.StrictMode
    Refactor main component to use App component with StrictMode.

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
+import App from './App'
 
-function App() {
-  return <h1>CVScore AI</h1>
-}
+createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
 
-createRoot(document.getElementById('root')!).render(<App />)
 
