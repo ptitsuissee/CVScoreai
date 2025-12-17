8b64c3a321d12d8b8f119868e5615aaff1038d38
ptitsuissee <xanos26@gmail.com>
2025-12-17 17:54:54 +0100

    Initialize React app with basic structure

--- src/main.tsx
+++ src/main.tsx
@@ -1,1 +1,9 @@
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+
+function App() {
+  return <h1>CVScore AI</h1>
+}
+
+createRoot(document.getElementById('root')!).render(<App />)
 
